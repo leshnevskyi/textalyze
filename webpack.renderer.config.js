890c0abf37f3881e00db53ca2b1406c23224f0d3
Leshnevskyi <leshnevskyi@gmail.com>
2021-11-01 15:46:28 +0200

    build: enable absolute imports

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
@@ -12,6 +13,7 @@ module.exports = {
   },
   plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
 };
