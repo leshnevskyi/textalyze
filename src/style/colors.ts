a819eeacdc70de85dd0615de358263d803841e5b
Leshnevskyi <leshnevskyi@gmail.com>
2021-11-01 15:45:28 +0200

    layout: add color constants

---
+++ src/style/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  white: '#FFFFFF',
+  whiteLinen: '#F9F1EB',
+  calico: '#DFB597',
+  brickRed: '#BD2F48',
+  craterBrown: '#452727',
+  aubergine: '#340B0B',
+};
+
+export default colors;
